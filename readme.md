02c5341d0ec1500e8275b046c8290d777e7c22bd
Rabia-Boota <irabiaboota@gmail.com>
2024-09-11 17:47:53 +0500

    Made Loader and Page1

---
+++ readme.md
@@ -0,0 +1,33 @@
+gsap
+scrolltrigger
+sheryjs
+script.js
+
+Final Project Introduction .......... 
+best digital creator - behtreen websites
+https://obys.agency/
+
+
+Final Project - Making Structure ..... 4:25
+
+
+Making Loader ...............9:20
+for what loader?
+jb tk sari website load ni ho jati, tb tk loader ko 2 3 seconds k lye dikha do, ta k bad mei ek pyara sa smooth sa experience mil jaye. kahin asa na ho k loader k bina koi user aye website py aur website/ web animation download e na ho payen to insult ho jaye.
+
+
+Now Animation............40:13
+
+how to upload on github ..... 48:10
+
+Applying Favicon............. 50:20
+
+Creating Page1.............. 55:42
+
+Cursor ................... 1:07:22
+
+Magnet Effect on Nav..........1:12:50
+
+search... npm > sheryjs >  css style sheet ko copy kr k style k upr paste kia, then js ko copy kr k script.js k upr paste kia then magnet wgera jo code uthany , wo utha k andr apny target k lye changing kr do.
+
+Creating Page1 (2) ............ 1:16:00
---
+++ script.js
@@ -0,0 +1,161 @@
+// LOADER EFFECT....... 17:02
+// GSAP lgaya, gsap cdn sy 1st and 2nd last wala copy kr k paste kia script k upr
+
+// is code ko neechy ly gaye
+// gsap.from(".line h1", {
+//     y:150,
+//     stagger:0.25,
+//     duration:0.6,
+//     delay:0.5
+// })
+
+// example 19:04
+
+// counter............25:04
+// setInterval name ki property hoti hai, ye ap k ksi b kaam ko ek particular interval k bad krti hai, 
+// setTimeout mtlb delay
+// setInterval mtlb loop ... mtlb ye ek triqy ka loop bnaty mgr ek particular interval k bad bnaty jesy
+
+// var h5timer = document.querySelector("#line1-part1 h5")
+// var grow = 0
+// setInterval(function(){
+//     // console.log("Hello")
+//     if(grow<100){
+//         // grow++
+//         // console.log(grow)
+//         h5timer.innerHTML = grow++
+//     } else{
+//         // grow = 100
+//         // console.log(grow)
+//         h5timer.innerHTML = grow
+//     }
+// }, 33)
+
+
+
+
+
+// var tl = gsap.timeline()
+// tl.from(".line h1", {
+//     y:150,
+//     stagger:0.25,
+//     duration:0.6,
+//     delay:0.5,
+// });
+// tl.from("#line1-part1",{
+//     opacity:0,
+//     onStart:function(){
+//         var h5timer = document.querySelector("#line1-part1 h5")
+// var grow = 0;
+// setInterval(function(){
+//     if(grow<100){
+//         h5timer.innerHTML = grow++
+//     } else{
+//         h5timer.innerHTML = grow
+//     }
+// }, 33)
+//     }
+// })
+// tl.to(".line h2",{
+//     animationName:"anime",
+//     opacity:1
+// })
+// tl.to("#loader", {
+//     opacity:0,
+//     duration:0.2,
+//     delay:4
+// })
+
+// // Completing Loader ........ 35:15
+// tl.from("#page1",{
+//     delay:0.2,
+//     y:1600,
+//     opacity:0,
+//     duration:0.5,
+//     ease:Power4
+// })
+// tl.to("#loader",{
+//     display:"none"
+// })
+
+// Applying Favicon............. 50:20
+// upr k sary code ko function mei daal dia k jb chahy call kr k chla lo, jb chahy call na kro.
+function loaderLoadingAnimation(){
+    var tl = gsap.timeline()
+tl.from(".line h1", {
+    y:150,
+    stagger:0.25,
+    duration:0.6,
+    delay:0.5,
+});
+tl.from("#line1-part1",{
+    opacity:0,
+    onStart:function(){
+        var h5timer = document.querySelector("#line1-part1 h5")
+var grow = 0;
+setInterval(function(){
+    if(grow<100){
+        h5timer.innerHTML = grow++
+    } else{
+        h5timer.innerHTML = grow
+    }
+}, 33)
+    }
+})
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1
+})
+tl.to("#loader", {
+    opacity:0,
+    duration:0.2,
+    delay:4
+})
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    // opacity:0,
+    duration:0.6,
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
+tl.from("#nav",{
+    opacity:0
+})    
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+
+// function heroLoadingAnimation(){
+//     var tl = gsap.timeline()
+// tl.from("#nav",{
+//     opacity:0
+// })    
+// tl.from(".hero h1",{
+//     y:120,
+//     stagger:0.2
+// })
+// }
+
+function cursorAnimation(){
+    // facicon.io py jaa k 
+document.addEventListener("mousemove",function(dets){
+    // console.log("hello")
+    // console.log(dets.x)
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+Shery.makeMagnet("#nav-part2 h4");
+}
+
+// heroLoadingAnimation();
+loaderLoadingAnimation();
+cursorAnimation();
